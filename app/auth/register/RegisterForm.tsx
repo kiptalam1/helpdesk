48427e88d5da42f2191dc0480ff94fb464edc383
kiptalam1 <adamskiptalam0@gmail.com>
2026-04-22 17:51:29 +0300

    implement user signup using supabase auth

--- app/auth/register/RegisterForm.tsx
+++ app/auth/register/RegisterForm.tsx
@@ -1,149 +1,199 @@
 "use client";
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, SyntheticEvent, useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { signUpSchema } from "@/lib/zod";
+import { z } from "zod";
+import { createClient } from "@/lib/supabase/client";
+import { toast } from "sonner";
+
+type ZodInputErrors = {
+  username?: string[],
+  email?: string[],
+  password?: string[],
+  confirmPassword?: string[],
+}
 
 function RegisterForm() {
-	const [showPassword, setShowPassword] = useState(false);
-	const [showConfirm, setShowConfirm] = useState(false);
-	const [errors, setErrors] = useState({
-		username: "",
-		email: "",
-		password: "",
-		confirmPassword: "",
-	});
-	const [loading, setLoading] = useState(false);
-	const [formData, setFormData] = useState({
-		username: "",
-		email: "",
-		password: "",
-		confirmPassword: "",
-	});
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [errors, setErrors] = useState<ZodInputErrors>({
+    username: [],
+    email: [],
+    password: [],
+    confirmPassword: [],
+  });
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-	function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
-		const { name, value } = e.target;
-		setFormData((prev) => ({ ...prev, [name]: value }));
-	}
+  const supabase = createClient();
 
-	return (
-		<div className="border border-border shadow-sm bg-card px-5 py-8 rounded-lg">
-			<form className="flex flex-col gap-4 w-full">
-				{/* username */}
-				<div className="flex flex-col gap-1.5">
-					<label htmlFor="username" className="text-sm font-medium">
-						Username
-					</label>
-					<input
-						required
-						autoComplete="name"
-						placeholder=" Junior Kotestes"
-						type="username"
-						name="username"
-						id="username"
-						value={formData.username}
-						onChange={handleInputChange}
-						className="border border-border rounded-lg bg-transparent  px-3 py-2 text-base outline-none w-full focus:border-primary focus:ring-2 focus:ring-primary/20 placeholder:text-sm placeholder:text-text-muted transition"
-					/>
-					{errors.username && (
-						<p className="text-xs text-red-500">{errors.username}</p>
-					)}
-				</div>
 
-				{/* email */}
-				<div className="flex flex-col gap-1.5">
-					<label htmlFor="email" className="text-sm font-medium">
-						Email
-					</label>
-					<input
-						required
-						autoComplete="email"
-						placeholder="you@example.com"
-						type="email"
-						name="email"
-						id="email"
-						value={formData.email}
-						onChange={handleInputChange}
-						className="border border-border rounded-lg bg-transparent  px-3 py-2 text-base outline-none w-full focus:border-primary focus:ring-2 focus:ring-primary/20 placeholder:text-sm placeholder:text-text-muted transition"
-					/>
-					{errors.email && (
-						<p className="text-xs text-red-500">{errors.email}</p>
-					)}
-				</div>
+  function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  }
 
-				{/* password */}
-				<div className=" flex flex-col gap-1.5">
-					<label htmlFor="password" className="text-sm font-semibold">
-						Password
-					</label>
-					<div className="relative">
-						<input
-							required
-							autoComplete="new-password"
-							placeholder="At least 6 characters (Uppercase, lowercase, number)"
-							type={showPassword ? "text" : "password"}
-							name="password"
-							id="password"
-							value={formData.password}
-							onChange={handleInputChange}
-							className="w-full rounded-lg border border-border bg-transparent px-3 py-2 pr-10 text-base outline-none transition focus:border-primary focus:ring-2 focus:ring-primary/20 placeholder:text-sm placeholder:text-text-muted"
-						/>
+  async function handleRegisterUser(e: SyntheticEvent) {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const parsed = signUpSchema.safeParse(formData);
+      if (!parsed.success) {
+        const { fieldErrors } = z.flattenError(parsed.error);
+        const errors = Object.values(fieldErrors);
+        console.warn("zoderrors: ", fieldErrors, errors);
+        setErrors(fieldErrors);
+        return;
+      }
 
-						<button
-							type="button"
-							aria-label="toggle password visibility"
-							onClick={() => setShowPassword((prev) => !prev)}
-							className="absolute inset-y-0 right-2 text-text-muted cursor-pointer hover:text-text duration-150 transition-colors">
-							{showPassword ?
-								<FaEyeSlash size={14} />
-							:	<FaEye size={14} />}
-						</button>
-					</div>
-					{errors.password && (
-						<p className="text-xs text-red-500">{errors.password}</p>
-					)}
-				</div>
+      const { error } = await supabase.auth.signUp({
+        email: formData.email,
+        password: formData.password,
+        options: {
+          data: {
+            full_name: formData.username,
+          }
+        }
+      })
+      if (error) {
+        toast.error(String(error.message));
+        // console.error("supabase errors: ", error);
+      } else {
+        //console.log(data);
+        toast.success("Account created successfully.");
+        //navigate to login page;
+      }
+    }
+    finally {
+      setLoading(false);
+    }
+  }
 
-				{/* confirm password */}
-				<div className=" flex flex-col gap-1.5">
-					<label htmlFor="confirmPassword" className="text-sm font-semibold">
-						Confirm Password
-					</label>
-					<div className="relative">
-						<input
-							required
-							autoComplete="new-password"
-							placeholder="Confirm your password ..."
-							type={showConfirm ? "text" : "password"}
-							name="confirmPassword"
-							id="confirmPassword"
-							value={formData.confirmPassword}
-							onChange={handleInputChange}
-							className="w-full rounded-lg border border-border bg-transparent px-3 py-2 pr-10 text-base outline-none transition focus:border-primary focus:ring-2 focus:ring-primary/20 placeholder:text-sm placeholder:text-text-muted"
-						/>
+  return (
+    <div className="border border-border shadow-sm bg-card px-5 py-8 rounded-lg">
+      <form onSubmit={handleRegisterUser} className="flex flex-col gap-4 w-full">
+        {/* username */}
+        <div className="flex flex-col gap-1.5">
+          <label htmlFor="username" className="text-sm font-medium">
+            Username
+          </label>
+          <input
+            required
+            autoComplete="name"
+            placeholder=" Junior Kotestes"
+            type="username"
+            name="username"
+            id="username"
+            value={formData.username}
+            onChange={handleInputChange}
+            className="border border-border rounded-lg bg-transparent  px-3 py-2 text-base outline-none w-full focus:border-primary focus:ring-2 focus:ring-primary/20 placeholder:text-sm placeholder:text-text-muted transition"
+          />
+          {errors.username && errors.username.map((error) => (
+            <p key={error} className="text-xs text-red-500">{error}</p>)
+          )}
+        </div>
 
-						<button
-							type="button"
-							aria-label="toggle password visibility"
-							onClick={() => setShowConfirm((prev) => !prev)}
-							className="absolute inset-y-0 right-2 text-text-muted cursor-pointer hover:text-text duration-150 transition-colors">
-							{showConfirm ?
-								<FaEyeSlash size={14} />
-							:	<FaEye size={14} />}
-						</button>
-					</div>
-					{errors.confirmPassword && (
-						<p className="text-xs text-red-500">{errors.confirmPassword}</p>
-					)}
-				</div>
+        {/* email */}
+        <div className="flex flex-col gap-1.5">
+          <label htmlFor="email" className="text-sm font-medium">
+            Email
+          </label>
+          <input
+            required
+            autoComplete="email"
+            placeholder="you@example.com"
+            type="email"
+            name="email"
+            id="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            className="border border-border rounded-lg bg-transparent  px-3 py-2 text-base outline-none w-full focus:border-primary focus:ring-2 focus:ring-primary/20 placeholder:text-sm placeholder:text-text-muted transition"
+          />
+          {errors.email && errors.email.map((error) => (
+            <p key={error} className="text-xs text-red-500">{error}</p>)
+          )}
+        </div>
 
-				<button
-					type="submit"
-					disabled={loading}
-					className="my-4 px-2 py-1 bg-primary rounded-md text-white font-semibold hover:opacity-70 transition-opacity duration-150 cursor-pointer">
-					{loading ? "Loading" : "Sign Up"}
-				</button>
-			</form>
-		</div>
-	);
+        {/* password */}
+        <div className=" flex flex-col gap-1.5">
+          <label htmlFor="password" className="text-sm font-semibold">
+            Password
+          </label>
+          <div className="relative">
+            <input
+              required
+              autoComplete="new-password"
+              placeholder="At least 6 characters (Uppercase, lowercase, number)"
+              type={showPassword ? "text" : "password"}
+              name="password"
+              id="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              className="w-full rounded-lg border border-border bg-transparent px-3 py-2 pr-10 text-base outline-none transition focus:border-primary focus:ring-2 focus:ring-primary/20 placeholder:text-sm placeholder:text-text-muted"
+            />
+
+            <button
+              type="button"
+              aria-label="toggle password visibility"
+              onClick={() => setShowPassword((prev) => !prev)}
+              className="absolute inset-y-0 right-2 text-text-muted cursor-pointer hover:text-text duration-150 transition-colors">
+              {showPassword ?
+                <FaEyeSlash size={14} />
+                : <FaEye size={14} />}
+            </button>
+          </div>
+          {errors.password && errors.password.map((error) => (
+            <p key={error} className="text-xs text-red-500">{error}</p>)
+          )}
+        </div>
+
+        {/* confirm password */}
+        <div className=" flex flex-col gap-1.5">
+          <label htmlFor="confirmPassword" className="text-sm font-semibold">
+            Confirm Password
+          </label>
+          <div className="relative">
+            <input
+              required
+              autoComplete="new-password"
+              placeholder="Confirm your password ..."
+              type={showConfirm ? "text" : "password"}
+              name="confirmPassword"
+              id="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleInputChange}
+              className="w-full rounded-lg border border-border bg-transparent px-3 py-2 pr-10 text-base outline-none transition focus:border-primary focus:ring-2 focus:ring-primary/20 placeholder:text-sm placeholder:text-text-muted"
+            />
+
+            <button
+              type="button"
+              aria-label="toggle password visibility"
+              onClick={() => setShowConfirm((prev) => !prev)}
+              className="absolute inset-y-0 right-2 text-text-muted cursor-pointer hover:text-text duration-150 transition-colors">
+              {showConfirm ?
+                <FaEyeSlash size={14} />
+                : <FaEye size={14} />}
+            </button>
+          </div>
+          {errors.confirmPassword && (
+            <p className="text-xs text-red-500">{errors.confirmPassword}</p>
+          )}
+        </div>
+
+        <button
+          type="submit"
+          disabled={loading}
+          className="my-4 px-2 py-1 bg-primary rounded-md text-white font-semibold hover:opacity-70 transition-opacity duration-150 cursor-pointer">
+          {loading ? "Loading" : "Sign Up"}
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterForm;
